7591b8973270a751c5a02b6d33d781f878b0c159
Lucas <lnmunhoz@Admins-MacBook-Pro.local>
2019-03-24 14:20:04 +0700

    Add git credential helper

--- terminal-setup.sh
+++ terminal-setup.sh
@@ -2,4 +2,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 
+git config --global credential.helper osxkeychain
+
 brew install yarn
